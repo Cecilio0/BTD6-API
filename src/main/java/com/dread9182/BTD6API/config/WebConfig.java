3e75bdd7eeaa43ff4f26efa32473cba9bb12c20e
Dread9182 <84605991+Dread9182@users.noreply.github.com>
2024-01-29 17:40:49 -0500

    Implemented UserAuthenticationRequestHandler

--- src/main/java/com/dread9182/BTD6API/config/WebConfig.java
+++ src/main/java/com/dread9182/BTD6API/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.dread9182.BTD6API.config;
 
+import com.dread9182.BTD6API.interceptor.UserAuthenticationRequestHandler;
 import com.dread9182.BTD6API.interceptor.UserRegisterRequestHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -10,5 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new UserRegisterRequestHandler()).addPathPatterns("/users/register");
+		registry.addInterceptor(new UserAuthenticationRequestHandler()).addPathPatterns("/users/authenticate");
 	}
 }
