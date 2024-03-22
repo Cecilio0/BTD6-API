93475698934c7f97a3bc1ac5ba6c75d0a62044cd
Dread9182 <84605991+Dread9182@users.noreply.github.com>
2024-03-21 20:58:52 -0500

    Added handler for BadCredentials

--- src/main/java/com/dread9182/BTD6API/exception/ApiExceptionHandler.java
+++ src/main/java/com/dread9182/BTD6API/exception/ApiExceptionHandler.java
@@ -1,14 +1,17 @@
 package com.dread9182.BTD6API.exception;
 
 import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ApiExceptionHandler {
 	private final HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+	private final HttpStatus forbidden = HttpStatus.FORBIDDEN;
 	
 	@ExceptionHandler(value = {NotFoundException.class})
 	public ResponseEntity<Object> handleNotFoundException(NotFoundException e){
@@ -42,4 +45,19 @@ public class ApiExceptionHandler {
 		
 		return new ResponseEntity<>(apiException, badRequest);
 	}
+	
+	@ExceptionHandler(value = {BadCredentialsException.class})
+	public ResponseEntity<Object> handleBadCredentialsException(BadCredentialsException e){
+		String message = e.getMessage();
+		
+		ApiException apiException = new ApiException(
+				message,
+				forbidden
+		);
+		
+		return new ResponseEntity<>(apiException, forbidden);
+	}
+	
+	// todo add handler for jwt exceptions
+	// check out https://medium.com/@mypascal2000/custom-handling-of-invalid-jwt-in-spring-boot-f66e60d59230
 }
